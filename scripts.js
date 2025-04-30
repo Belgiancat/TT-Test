3c21ffe8e09777f65dff350d2b5256374d410977
Jasper Nöldeke <104715499+TMmind.@users.noreply.github.com>
2025-04-30 18:02:12 +0200

    test2

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 //Antworten Empfangen
 
+let data = {}
+
 window.addEventListener("message", (event) => {
-  document.querySelector("#result").innerHTML = JSON.stringify(event.data, null, 2)
+  if (!Array.isArray(event.data.data)) {
+    data = { ...data, ...event.data.data }
+    document.querySelector("#result").innerHTML = JSON.stringify(data, null, 2)
+  }
 })
 
 document.querySelector("#test").addEventListener("click", () => {
-  window.parent.postMessage({ type: "getData" })
+  window.parent.postMessage({ type: "getData" }, "*")
 })
+
